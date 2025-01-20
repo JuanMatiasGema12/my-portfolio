d2394a0445064e35554ece0f3b49cf83fe3735be
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2025-01-20 17:30:28 -0300

    Adjusting some styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,13 +6,14 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
   const toggleMenu = () => {
-    setMenuOpen(true);
+    setMenuOpen((prevState) => !prevState);
   };
 
   return (
     <nav className="max-w-[1300px] mx-auto px-4 w-full fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2">
-      <div className="flex justify-between w-full max-w-[1200px] mx-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-orange">
+      <div className="flex justify-between w-full max-w-[1200px] mx-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-darkOrange">
         <NavbarLogo></NavbarLogo>
 
         <div className={`${menuOpen ? "sm:block" : "sm:hidden"} lg:block `}>
@@ -21,9 +22,9 @@ export const Navbar = () => {
 
         <NavbarBtn></NavbarBtn>
       </div>
-      <div className="flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full border-[0.5px] border-orange ">
+      <div className="flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full border-[0.5px] border-darkOrange ">
         <button
-          className="text-xl p-3 border border-orange rounded-full text-white "
+          className="text-2xl p-3 border border-darkOrange rounded-full text-white "
           onClick={toggleMenu}
         >
           <GiHamburgerMenu />
